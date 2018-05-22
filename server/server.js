2bd62a8da64c7a1e72f88b01d23445f9f187eb63
Mohd Shahmi Abdul Majid <mshahmi@moneylion.com>
2018-05-23 07:36:27 +0800

    Implemented uuid

--- server/server.js
+++ server/server.js
@@ -1,12 +1,15 @@
 //npm modules
 const express = require('express');
+const uuid = require('uuid/v4');
 
 // create the server
 const app = express();
 
 // create the homepage route at '/'
 app.get('/', (req, res) => {
-    res.send('you just hit the home page\n')
+    console.log(req)
+    const uniqueId = uuid()
+    res.send(`Hit home page. Received the unique id: ${uniqueId}\n`)
   })
 
 // tell the server what port to listen on
