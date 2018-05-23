cc7a57ca1e6192e3be737b49534606866f351374
Mohd Shahmi Abdul Majid <mshahmi@moneylion.com>
2018-05-23 09:06:57 +0800

    Added body-parser

--- server/server.js
+++ server/server.js
@@ -26,6 +26,19 @@ app.get('/', (req, res) => {
     console.log(req.sessionID)
     res.send(`You hit home page!\n`)
   })
+  
+// create the login get and post routes
+app.get('/login', (req, res) => {
+    console.log('Inside GET /login callback function')
+    console.log(req.sessionID)
+    res.send(`You got the login page!\n`)
+  })
+  
+  app.post('/login', (req, res) => {
+    console.log('Inside POST /login callback function')
+    console.log(req.body)
+    res.send(`You posted to the login page!\n`)
+  })
 
 // tell the server what port to listen on
 app.listen(3000, () => {
